3be5e4b4ab3ea44a11647eee4a5ce06957be27af
kms <do.88000@gmail.com>
2025-03-23 19:08:24 +0300

    8. Date. Отображение даты. [#310017]

--- src/main/java/ru/job4j/encapsulation/Config.java
+++ src/main/java/ru/job4j/encapsulation/Config.java
@@ -8,12 +8,15 @@ public class Config {
     public Config(String name) {
         this.name = name;
     }
+
     public void print() {
         System.out.println(position);
     }
+
     public String getProperty(String key) {
         return search(key);
     }
+
     private String search(String key) {
         return key;
     }
