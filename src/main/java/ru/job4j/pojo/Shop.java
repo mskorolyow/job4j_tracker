357480b691a27fa820fdc2e0ddfd9ff3d1a1a67e
kms <do.88000@gmail.com>
2025-03-24 20:59:54 +0300

    3.1. Массив с пустыми ячейками [#176470]

---
+++ src/main/java/ru/job4j/pojo/Shop.java
@@ -0,0 +1,28 @@
+package ru.job4j.pojo;
+
+public class Shop {
+    public static void main(String[] args) {
+        Product[] product = new Product[5];
+        product[0] = new Product("Milk", 10);
+        product[1] = new Product("Bread", 4);
+        product[2] = new Product("Egg", 19);
+        for (Product prod :
+                product) {
+            if (prod != null) {
+                System.out.println(prod.getName());
+            }
+        }
+    }
+
+    public static int indexOfNull(Product[] products) {
+        int result = -1;
+        for (int i = 0; i < products.length; i++) {
+            Product product = products[i];
+            if (product == null) {
+                result = i;
+                break;
+            }
+        }
+        return result;
+    }
+}
