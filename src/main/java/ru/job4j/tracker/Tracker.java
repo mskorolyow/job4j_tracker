4170837e8715d2500dd60e2c5e13e409d88077d8
kms <do.88000@gmail.com>
2025-04-01 21:08:27 +0300

    5. Tracker - хранилище [#396]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -1,5 +1,7 @@
 package ru.job4j.tracker;
 
+import java.util.Arrays;
+
 public class Tracker {
     private final Item[] items = new Item[100];
     private int ids = 1;
@@ -12,14 +14,34 @@ public class Tracker {
     }
 
     public Item findById(int id) {
-        Item rsl = null;
+        Item result = null;
         for (int index = 0; index < size; index++) {
             Item item = items[index];
             if (item.getId() == id) {
-                rsl = item;
+                result = item;
                 break;
             }
         }
-        return rsl;
+        return result;
     }
+
+    public Item[] findAll() {
+        return Arrays.copyOf(items, size);
+    }
+
+    public Item[] findByName(String key) {
+        Item[] result = new Item[items.length];
+        int j = 0;
+        for (int i = 0; i < size; i++) {
+            if (items[i] != null) {
+                Item item = items[i];
+                if (item.getName().equals(key)) {
+                    result[j] = item;
+                    j++;
+                }
+            }
+        }
+        return Arrays.copyOf(result, j);
+    }
+
 }
